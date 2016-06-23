0e67ff15b92d935e4d12525983b0c761befb4d6e
napjon <napitupulu.jon@gmail.com>
2016-06-23 17:34:29 +0700

    Adding stacked bar chart

--- chart.py
+++ chart.py
@@ -10,36 +10,76 @@ RESET_OPTION = """
 APPEND_ELEMENT = """
 $('#{id}').attr('id','{id}'+'_old');
 element.append('<div id="{id}" style="width: 600px;height:400px;"></div>');"""
-def generate_new_chartId():
-    global i
-    i+=1
-    return 'charts{}'.format(i)
-i = 25
+
+OPTION_TEMPLATE = {
+        'title': {
+            'text': ''
+        },
+        'tooltip': {'axisPointer':{'type':''}},
+        'legend': {
+            'data':[]
+        },
+        'xAxis': {
+            'data': []
+        },
+        'yAxis': {},
+        'series': []
+    }
+
 class Chart():
     def __init__(self,**kwargs):
- 
         self._chartId = str(uuid.uuid4())
+        self._option_ = deepcopy(OPTION_TEMPLATE)
+        
+        
     def resync_data(data):
         return self
+    
+    
     def set_legend(self,):
         pass
+    
+    
     def set_theme(self,):
         pass
+    
+    
     def resync_data(self,option):
         return Javascript(self._get_resync_data_strings(option))
+    
+    
     def _get_resync_data_strings(self,option):
         return RESET_OPTION % (self._chartId,json.dumps(option))
-    def set_tooltip(self):
-        pass
+    
+    
+    def set_tooltip(self,trigger='axis',axis_pointer='shadow'):
+        self._option_['tooltip']['trigger'] = trigger
+        self._option_['tooltip']['axisPointer']['type'] = axis_pointer
+        return self
+    
+    
     def get_option(self):
         global optionx
         return option
+    
+    
+    def set_title(self,s):
+        self._option_['title']['text'] = s
+        return self
+    
+    
+    def flip_axes(self):
+        self._axes_swapped = not self._axes_swapped
+        self._option_['xAxis'],self._option_['yAxis'] = self._option_['yAxis'],self._option_['xAxis']
+        return self
+    
+    
     def _repr_javascript_(self):
         return (APPEND_ELEMENT.format(id=self._chartId))+\
-                (self._get_resync_data_strings(option))
+                (self._get_resync_data_strings(self._option_))
+        
+    _axes_swapped = True
         
         #return RESET_OPTION % (self._chartId,json.dumps(option))
     
     
-def append(chartId):
-    return Javascript("""element.append('<div id="%s" style="width: 600px;height:200px;"></div>');"""%chartId)
--- plot.py
+++ plot.py
@@ -4,15 +4,42 @@ def make_chart(**kwargs):
     
     c = Chart()
     
-    data = kwargs['data']
-    opt_data = data[kwargs['x']].value_counts()
+    df = kwargs['data']
+    elem_series = {
+            'name': '',
+            'type': kwargs['type'],
+            'data': []}
+    
+    x = kwargs['x']
+    y = kwargs['y']
+    category = kwargs['category']
     
     
     if kwargs['type'] == 'bar':
-        c._option_['xAxis']['data'] = opt_data.index.tolist()
-        c._option_['series'][0]['data'] = opt_data.values.tolist()
-        c._option_['series'][0]['name'] = kwargs['x']
-        c._option_['legend']['data'].append(kwargs['x'])
+        if category:
+            c._option_['xAxis']['data'] = df[x].unique().tolist()
+            c._option_['legend']['data'] = df[category].unique().tolist()
+            
+            #Iterate and append Data for every category
+            for cat, subset in df.groupby(category):
+                opt_data = subset.ix[subset[category] == cat,x].value_counts()
+                d_series = deepcopy(elem_series)
+                d_series['name'] = str(cat)
+                d_series['data'] = opt_data.values.tolist()
+                
+                if kwargs['stacked'] == True:
+                    d_series['stack'] = category
+                c._option_['series'].append(d_series)
+        else:
+            
+            opt_data = df[x].value_counts()
+            c._option_['xAxis']['data'] = opt_data.index.tolist()
+            d_series = elem_series.copy()
+            d_series['data'] = opt_data.values.tolist()
+            d_series['name'] = x
+            
+            c._option_['series'].append(d_series)
+            c._option_['legend']['data'].append(x)
         
     
     return c
@@ -20,15 +47,15 @@ def make_chart(**kwargs):
         
     
 
-def bar(data,x=None,y=None,category=None,how='count',**kwargs):
+def bar(df,x=None,y=None,category=None,how='count',stacked=False,**kwargs):
     
     kwargs['x'] = x
     kwargs['y'] = y
-    kwargs['data'] = data
+    kwargs['data'] = df
     kwargs['category'] = category
     kwargs['how'] = how
     kwargs['type'] = 'bar'
+    kwargs['stacked'] = stacked
     
     return make_chart(**kwargs)
-    
     
