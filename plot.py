f38db004fea57b102a1785359cef2bcccd4e1fb9
napjon <napitupulu.jon@gmail.com>
2016-06-28 11:47:47 +0700

    Refactor series

--- plot.py
+++ plot.py
@@ -1,6 +1,57 @@
 
 #from . import Chart
+
+def bar_line_hist_condition(c,category=None,**kwargs):
+    """Provide stacked,annotate, area for bar line hist"""
+    series = c._option['series']
+
+    d_annotate = {'normal':{'show':True,
+                            'position':'top'}}
+
+    if category and kwargs['stacked'] == True:
+        for s in series:
+            s['stack'] = category
+
+            if kwargs['type'] == 'line' and kwargs['area'] == True:
+                s['areaStyle'] = {'normal': {}}
+
+            if kwargs['annotate'] == 'all':
+                s['label'] = d_annotate
+
+    if kwargs['annotate'] == 'top':
+        series[-1]['label'] = d_annotate
+        
+        
+def round_list(arr):
+    try:
+        return arr.values.round(3).tolist()
+    except TypeError:
+        return arr.unique().tolist()
+    
+                
 def make_chart(df,**kwargs):
+    
+    def insert_series_on(f):
+
+        if category:
+            #Iterate and append Data for every category
+            for cat, subset in df.groupby(category):
+                cat = str(cat)
+                insert_data_to_series(f,subset,cat)
+                c._option['legend']['data'].append(cat)
+        else:
+            insert_data_to_series(f,df)
+    
+    
+    def insert_data_to_series(f,df,cat=None):
+        data = f(df)
+        
+        series = deepcopy(elem_series)
+        series['data'] = round_list(data)
+        series['type'] = kwargs['type']
+        series['name'] = cat if cat else x
+        c._option['series'].append(series)
+        
     
     c = Chart(**kwargs)
     
@@ -13,56 +64,27 @@ def make_chart(df,**kwargs):
     y = kwargs.get('y')
     category = kwargs['category']
     
-    def round_list(arr):
-        try:
-            return arr.values.round(3).tolist()
-        except TypeError:
-            return arr.unique().tolist()
-    
-    
     if kwargs['type'] in ['bar','line']:
         
         
-        def insert_series(df,cat=None):
-            """Return data series based on dataframe"""
+        def get_bar_line_data(df):
             c._option['xAxis']['data'] = round_list(df[x].drop_duplicates())
             
             data = (df[x].value_counts()
-                        if y is None else
-                        df.groupby(x)[y].aggregate(kwargs['how']))
+                    if y is None else
+                    df.groupby(x)[y].aggregate(kwargs['how']))
             
+            return data
             
-            series = deepcopy(elem_series)
-            series['data'] = round_list(data)
-            series['type'] = kwargs['type']
-            
-            if kwargs['stacked'] == True:
-                series['stack'] = category
-                    
-            series['name'] = cat if cat else x
-            c._option['series'].append(series)
         
-        
-        if category:
-            #Iterate and append Data for every category
-            for cat, subset in df.groupby(category):
-                insert_series(subset,cat=cat)
-                c._option['legend']['data'].append(cat)
-        else:
-            insert_series(df)
-            
-        if (category and
-            kwargs['type'] == 'line' and 
-            kwargs['area'] == True and 
-            kwargs['stacked']== True):
-            for e in c._option['series']:
-                e['areaStyle']= {'normal': {}}
+        insert_series_on(get_bar_line_data)
+        bar_line_hist_condition(c,**kwargs)
             
     elif kwargs['type'] == 'hist':
         kwargs['type'] = 'bar'
         
         
-        def insert_series(df,cat=None):
+        def get_hist_data(df):
             
             y_val,x_val = np.histogram(df[x],
                                        bins=kwargs['bins'],
@@ -71,44 +93,27 @@ def make_chart(df,**kwargs):
             bins = x_val.astype(int).tolist()
             c._option['xAxis']['data'] = bins
             
-            series = deepcopy(elem_series)
-            series['data'] = round_list(data)
-            series['type'] = kwargs['type']
-            series['name'] = cat if cat else x
-                
-            if kwargs['stacked'] == True:
-                series['stack'] = category
-            
-            c._option['series'].append(series)
-            
-            
-        if category:
-            for cat,subset in df.groupby(category):
-                insert_series(subset,cat=cat)
-                c._option['legend']['data'].append(cat)
-        else:
-            insert_series(df)
-#             c._option['xAxis']['data'] =  get_hist_series(df,x,**kwargs)
+            return data
+        
+        insert_series_on(get_hist_data)
+        bar_line_hist_condition(c,**kwargs)
             
     elif kwargs['type'] == 'scatter':
         
         c._option['xAxis'] = {'type': 'value',
                               'name': x,
                               'max': int(df[x].max())}
-        
         c._option['yAxis'] = {'type': 'value',
                               'name': y,
                               'max': int(df[y].max())}
-        
-        
-        cols = [x,y]
-     
         c._option['visualMap'] = []
+        
         visual_map_template = {'show': False,
                                'min': 0,
                                'max': 999,
                                'inRange': {}}
         
+        cols = [x,y]
         size = kwargs['size']
         if size is not None:
             vmap_size = deepcopy(visual_map_template)
@@ -117,6 +122,7 @@ def make_chart(df,**kwargs):
             vmap_size['inRange']['symbolSize'] = [6,60]
             c._option['visualMap'].append(vmap_size)
             cols.append(size)
+        
             
         #TODO: Fix Saturate
 #          saturate = kwargs['saturate']
@@ -128,28 +134,19 @@ def make_chart(df,**kwargs):
 #             c._option['visualMap'].append(vmap_saturate)
 #             cols.append(saturate)
 
-        def insert_series(df,cat=None):
+
+        def get_scatter_data(df):
         
             data = df[cols]
-            series = deepcopy(elem_series)
-            series['data'] = round_list(data)
-            series['type'] = kwargs['type']
-            series['name'] = cat if cat else x
-            c._option['series'].append(series)
-            
-            
-        if category:
-            for cat,subset in df.groupby(category):
-                insert_series(subset,cat=cat)
-                c._option['legend']['data'].append(cat)
-        else:
-            insert_series(df)
-    
+            return data
+        
+        insert_series_on(get_scatter_data)
             
     return c
 
 
-def bar(df,x,y=None,category=None,how='count',stacked=False,**kwargs):
+def bar(df,x,y=None,category=None,how='count',stacked=False,
+        annotate=None,**kwargs):
     
     kwargs['x'] = x
     kwargs['y'] = y
@@ -157,10 +154,12 @@ def bar(df,x,y=None,category=None,how='count',stacked=False,**kwargs):
     kwargs['how'] = how
     kwargs['type'] = 'bar'
     kwargs['stacked'] = stacked
+    kwargs['annotate'] = 'top' if annotate == True else annotate
     
     return make_chart(df,**kwargs)
 
-def line(df,x,y=None,category=None,how='count',stacked=False,area=False,**kwargs):
+def line(df,x,y=None,category=None,how='count',stacked=False,area=False,
+         annotate=None,**kwargs):
     
     kwargs['x'] = x
     kwargs['y'] = y
@@ -169,10 +168,12 @@ def line(df,x,y=None,category=None,how='count',stacked=False,area=False,**kwargs
     kwargs['type'] = 'line'
     kwargs['stacked'] = stacked
     kwargs['area'] = area
+    kwargs['annotate'] = 'top' if annotate == True else annotate
     
     return make_chart(df,**kwargs)
 
-def hist(df,x,category=None,bins=10,normed=False,stacked=False,**kwargs):
+def hist(df,x,category=None,bins=10,normed=False,stacked=False,
+         annotate=None,**kwargs):
     
     kwargs['x'] = x
     kwargs['category'] = category
@@ -180,6 +181,7 @@ def hist(df,x,category=None,bins=10,normed=False,stacked=False,**kwargs):
     kwargs['type'] = 'hist'
     kwargs['normed'] = normed
     kwargs['stacked'] = stacked
+    kwargs['annotate'] = 'top' if annotate == True else annotate
     
     return make_chart(df,**kwargs)
 
