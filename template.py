ec2fb8bd93645af3eccb814bccd29fc5ac3146ba
napjon <napitupulu.jon@gmail.com>
2016-06-30 20:49:08 +0700

    Added JS library

---
+++ template.py
@@ -0,0 +1,87 @@
+
+def save_html(script,path):
+    from jinja2 import Template
+    html_template = open('template.html','r')
+    script = script.replace('element','$("body")')
+    f = open(path,'w')
+    f.write(Template(html_template.read()).render(SCRIPT=script))
+    f.close()
+    html_template.close()
+
+
+RESET_OPTION = """
+require({requires},function(echarts){{
+    
+    function parseFunction(str){{
+        return eval('(' + str + ')');
+    }}
+    
+    var myChart = echarts.init(document.getElementById("{chartId}"),"{theme}");
+    
+    var option = {option};
+    option['tooltip']['formatter'] = parseFunction(option['tooltip']['formatter']);
+    
+    //option['series'][0]['symbolSize'] = function (val){{return val[2]*10;}}
+    
+    
+    myChart.setOption(option);
+    //console.log(option);
+    {events}
+    
+}});
+"""
+
+
+APPEND_ELEMENT = """
+$('#{id}').attr('id','{id}'+'_old');
+element.append('<div id="{id}" style="width: 600px;height:400px;"></div>');"""
+
+
+OPTION_TEMPLATE = {
+        'title': {
+            'text': ''
+        },
+        'tooltip': {'axisPointer':{'type':''}},
+        'legend': {
+            'data':[]
+        },
+        'xAxis': {
+            'data': []
+        },
+        'yAxis': {},
+        'series': []
+    }
+
+
+EVENTS_TEMPLATE = """
+myChart.on('{event}',function(params){{
+
+    var d_params  = {{'series':{{'name':params.seriesName,
+                                 'index':params.seriesIndex}},
+                      'data':{{'value':params.value,
+                               'index':params.dataIndex,
+                               'name':params.name}}
+    }}
+    
+    console.log('parameters extracted: ');
+    console.log(d_params);
+    
+    // Create new cell and execute function passed with parameters
+    var nb = Jupyter.notebook;
+    nb.insert_cell_below();
+    nb.select_next();
+    
+    var json_strings = JSON.stringify(d_params);
+
+    var cell = nb.get_selected_cell();
+    var code_input = "{function}(json.loads('" + json_strings + "'))";
+    console.log("Executing code: " + code_input);
+    cell.set_text(code_input);
+    cell.execute();
+    
+    
+    // Immediately delete the cell after execute
+    nb.delete_cell();
+    
+}});
+"""
