0a2b3b183f8085a010591f161b55c81b7b34c4e1
jonhari <napitupulu.jon@gmail.com>
2016-07-06 09:17:35 +0700

    Fix read_df bug

--- krisk/connections.py
+++ krisk/connections.py
@@ -2,14 +2,15 @@
 from collections import OrderedDict
 from IPython.display import Javascript
 import json
+from krisk.util import join_current_dir
 
 ECHARTS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/echarts/3.2.0/'
 ECHARTS_FILE = 'echarts.min'
 d_paths = OrderedDict({})
 THEMES = ['dark','vintage','roma','shine','infographic','macarons']
 THEMES_URL='//echarts.baidu.com/asset/theme/'
-PATH_LOCAL = 'krisk/static'
-
+PATH_LOCAL = 'pandas-echarts/krisk/static'
+#TODO FIX LOCAL PATH!
 
 def init_notebook(online=True):
     """Inject Javascript to notebook, default using local js.
