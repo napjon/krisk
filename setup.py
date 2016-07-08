ebc34c17941ed9fef30f3cfa9eacdea2c6076b64
napjon <napitupulu.jon@gmail.com>
2016-07-08 14:27:43 +0700

    Bump version to v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup,find_packages
 
 setup(name='krisk',
-      version='0.1.0',
+      version='0.1.1',
       description='Echarts Statistical Visualization for Python Data Science',
       author='Jonathan Napitupulu',
       author_email='napitupulu.jon@gmail.com',
