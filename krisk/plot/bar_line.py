f866401c076df3e720695494b24f25f9ef472bc2
napjon <harijh2@gmail.com>
2016-08-30 18:01:11 +0700

    Refactor plot.py as package, fix #30 (#33)
    * Separate bar_line, scatter to its own module
    * make_chart.py now under plot package
    * plot.py change to plot/__init__.py

---
+++ krisk/plot/bar_line.py
@@ -0,0 +1,90 @@
+from copy import deepcopy
+import numpy as np
+import pandas as pd
+
+from krisk.plot.make_chart import insert_series_data
+
+
+def set_bar_line_chart(c, df, x, category, **kwargs):
+
+    data = None
+    chart_type = kwargs['type']
+
+    if chart_type in ['bar', 'line']:
+        data = get_bar_line_data(df, x, category, **kwargs)
+        c._option['xAxis']['data'] = data.index.values.tolist()
+
+    elif chart_type == 'hist':
+        chart_type = 'bar'
+        data, bins = get_hist_data(df, x, category, **kwargs)
+        c._option['xAxis']['data'] = bins
+
+    if category:
+        # append data for every category
+        for cat in data.columns:
+            insert_series_data(data[cat], x, chart_type, c, cat)
+    else:
+        insert_series_data(data, x, chart_type, c)
+
+    series = c._option['series']
+
+    ########Provide stacked,annotate, area for bar line hist#################
+    d_annotate = {'normal': {'show': True, 'position': 'top'}}
+
+    if category and kwargs['stacked']:
+        for s in series:
+            s['stack'] = category
+
+            if chart_type == 'line' and kwargs['area']:
+                s['areaStyle'] = {'normal': {}}
+
+            if kwargs['annotate'] == 'all':
+
+                if chart_type == 'bar':
+                    d_ant = deepcopy(d_annotate)
+                    d_ant['normal']['position'] = 'inside'
+                    s['label'] = deepcopy(d_ant)
+                else:
+                    s['label'] = deepcopy(d_annotate)
+
+    if kwargs['annotate'] == 'top':
+        series[-1]['label'] = d_annotate
+    # TODO: make annotate receive all kinds supported in echarts.
+
+
+def get_bar_line_data(df, x, category, y, **kwargs):
+
+    if category:
+        if y is None:
+            data = pd.crosstab(df[x], df[category])
+        else:
+            data = df.pivot_table(
+                index=x,
+                values=y,
+                columns=category,
+                aggfunc=kwargs['how'],
+                fill_value=0)
+    else:
+        if y is None:
+            data = df[x].value_counts()
+        else:
+            raise AssertionError('Use y in category instead')
+
+    return data
+
+
+def get_hist_data(df, x, category, **kwargs):
+
+    y_val, x_val = np.histogram(
+        df[x], bins=kwargs['bins'], normed=kwargs['normed'])
+    bins = x_val.astype(int).tolist()
+
+    if category:
+        data = pd.DataFrame()
+        for cat, sub in df.groupby(category):
+            data[cat] = (pd.cut(sub[x], x_val).value_counts(
+                sort=False, normalize=kwargs['normed']))
+    else:
+        data = pd.Series(y_val)
+
+    return data, bins
--- krisk/util.py
+++ krisk/util.py
@@ -1,4 +1,7 @@
+import os
+from copy import deepcopy
+
+
 def join_current_dir(file):
-    import os
     cwd = os.path.dirname(__file__)
     return os.path.join(cwd, file)
