893f91c5b0a0a6d965bde8dc0a6b6b0a12b27bde
napjon <harijh2@gmail.com>
2016-11-02 18:05:54 +0700

    Bump 0.2.1, Fix FileNotFoundError

--- krisk/__init__.py
+++ krisk/__init__.py
@@ -6,7 +6,7 @@ import krisk.plot.api as plot
 import krisk.chart.api as chart
 from krisk.chart.api import rcParams, Chart
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 def _jupyter_nbextension_paths():
