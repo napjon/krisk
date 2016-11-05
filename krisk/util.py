cbf39a0b5f2876a7cf1ce45361be3ba79aba4dc7
napjon <napitupulu.jon@gmail.com>
2016-11-05 14:31:58 +0700

    fix util.py indentation

--- krisk/util.py
+++ krisk/util.py
@@ -4,16 +4,16 @@ import codecs
 
 
 def join_current_dir(file):
-	"""Join filepath with current file directory"""
-	cwd = os.path.dirname(__file__)
-	return os.path.join(cwd, file)
+    """Join filepath with current file directory"""
+    cwd = os.path.dirname(__file__)
+    return os.path.join(cwd, file)
 
 
 def get_content(filepath):
-	"""Retrieve content from file"""
-	abs_path = join_current_dir(filepath)
-	with open(abs_path, 'r') as f:
-		return f.read()
+    """Retrieve content from file"""
+    abs_path = join_current_dir(filepath)
+    with open(abs_path, 'r') as f:
+        return f.read()
 
 
 def init_notebook():
